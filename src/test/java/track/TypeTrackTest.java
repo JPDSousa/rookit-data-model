5053fc86c02e575f0bff6ab24065823a0be6dc25
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-15 10:03:03 +0100

    additional tests for TypeTrack

--- src/test/java/track/TypeTrackTest.java
+++ src/test/java/track/TypeTrackTest.java
@@ -26,14 +26,38 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.rookit.dm.track.TypeTrack;
 
+import static org.rookit.dm.track.TypeTrack.*;
+
 @SuppressWarnings("javadoc")
 public class TypeTrackTest {
 	
 	@Test
 	public void testTrackClass(){
-		for(TypeTrack t : TypeTrack.values()){
+		for(TypeTrack t : values()){
 			assertNotNull(TypeTrack.class.getName()+" "+t.name()+"'s track class is not defined!", t.getTrackClass());
 		}
+	}
+	
+	@Test
+	public final void testGetByClass() {
+		for(TypeTrack t : values()) {
+			assertEquals(t, getByClass(t.getTrackClass()));
+		}
+	}
+	
+	@Test
+	public final void testGetName() {
+		for(TypeTrack t : values()) {
+			assertNotNull(t.getName());
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public final void testGetByName() {
+		assertEquals(ORIGINAL, getByName("ORIGINAL"));
+		assertEquals(VERSION, getByName("REMIX"));
+		assertEquals(VERSION, getByName("COVER"));
 	}
 
 }
