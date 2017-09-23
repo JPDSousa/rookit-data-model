20a39205fff5299ad05015f2cd6c058b2dddc9bd
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-23 13:20:55 +0100

    moved playable

--- src/main/java/org/rookit/dm/play/Playable.java
+++ src/main/java/org/rookit/dm/play/Playable.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  ******************************************************************************/
-package org.rookit.dm.playable;
+package org.rookit.dm.play;
 
 import java.time.LocalDate;
 
