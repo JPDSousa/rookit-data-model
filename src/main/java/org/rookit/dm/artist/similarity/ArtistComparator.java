1e22378621f8cde41c42539c281a55b68b6a3c99
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-09 12:43:50 +0000

    ArtistComparator

---
+++ src/main/java/org/rookit/dm/artist/similarity/ArtistComparator.java
@@ -0,0 +1,56 @@
+package org.rookit.dm.artist.similarity;
+
+import static org.rookit.dm.artist.DatabaseFields.*;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Collections;
+import java.util.Map;
+
+import org.rookit.dm.artist.Artist;
+import org.rookit.dm.genre.similarity.AbstractGenreableComparator;
+
+@SuppressWarnings("javadoc")
+public class ArtistComparator extends AbstractGenreableComparator<Artist> {
+
+	public ArtistComparator() {
+		this(DEFAULT_THESHOLD);
+	}
+
+	public ArtistComparator(int threshold) {
+		this(threshold, Collections.emptyMap());
+	}
+	
+	public ArtistComparator(Map<String, Float> percentages) {
+		this(DEFAULT_THESHOLD, percentages);
+	}
+
+	public ArtistComparator(int threshold, Map<String, Float> percentages) {
+		super(threshold, percentages);
+	}
+
+	@Override
+	protected Map<String, Integer> createTopMap(Artist element1, Artist element2) {
+		final Map<String, Integer> scores = super.createTopMap(element1, element2);
+		scores.put(ALIASES, reverseIntersect(element1.getAliases(), element2.getAliases()));
+		scores.put(BEGIN_DATE, compareDate(element1.getBeginDate(), element2.getBeginDate()));
+		scores.put(END_DATE, compareDate(element1.getEndDate(), element2.getEndDate()));
+		scores.put(IPI, compareFromEquals(element1.getIPI(), element2.getIPI()));
+		scores.put(ISNI, compareFromEquals(element1.getISNI(), element2.getISNI()));
+		scores.put(NAME, compareStringIgnoreCase(element1.getName(), element2.getName()));
+		scores.put(ORIGIN, compareFromEquals(element1.getOrigin(), element2.getOrigin()));
+		scores.put(TYPE, compareFromEquals(element1.getType(), element2.getType()));
+		return scores;
+	}
+
+	private Integer compareDate(LocalDate beginDate, LocalDate beginDate2) {
+		final Period period = Period.between(beginDate, beginDate2);
+		final long totalMonths = Math.abs(period.toTotalMonths());
+		final int maxMonths = 12;
+		if(totalMonths > maxMonths) {
+			return threshold;
+		}
+		return (int) (totalMonths*threshold/maxMonths);
+	}
+
+}
