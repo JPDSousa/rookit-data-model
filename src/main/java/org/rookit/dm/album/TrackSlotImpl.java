73a8777fb3f93cd8d04cefd55bdbb5b81e5577b0
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-12-02 16:26:22 +0000

    track slot

---
+++ src/main/java/org/rookit/dm/album/TrackSlotImpl.java
@@ -0,0 +1,35 @@
+package org.rookit.dm.album;
+
+import org.rookit.dm.track.Track;
+
+@SuppressWarnings("javadoc")
+public class TrackSlotImpl implements TrackSlot {
+
+	private final String disc;
+	private final int number;
+	private final Track track;
+	
+	public TrackSlotImpl(String disc, int number, Track track) {
+		super();
+		this.disc = disc;
+		this.number = number;
+		this.track = track;
+	}
+
+	@Override
+	public String getDisc() {
+		return disc;
+	}
+
+	@Override
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public Track getTrack() {
+		return track;
+	}
+	
+	
+}
